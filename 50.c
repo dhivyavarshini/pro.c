fa31f051db35db5030d562260897063883cd82b8
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-01-29 06:20:15 -0800

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+	int i,a;
+	scanf("%d",&n);
+	for(i=1;i<=a;i++)
+	{
+	if(pow(2,i)==a)
+	{
+	printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+
+}
+}
