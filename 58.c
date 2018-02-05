2e4bf6975c871c4bc68f831e657ec01b16ecd495
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-02-05 16:10:49 +0530

    Update 58.c

--- 58.c
+++ 58.c
@@ -1,11 +1,11 @@
     #include <stdio.h>
-     
-    int main(void) {
-    	int a,b;
-    	scanf("%d %d",&a,&b);
-    	a=a^b;
-    	b=a^b;
-    	a=a^b;
-    	printf("%d %d",a,b);
-    	return 0;
-    }
+
+int main(void) {
+	int a1,b1;
+	scanf("%d %d",&a1,&b1);
+	a1=a1^b1;
+	b1=a1^b1;
+	a1=a1^b1;
+	printf("%d %d",a1,b1);
+	return 0;
+}
