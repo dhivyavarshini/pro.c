28f387e5576b52dd26053141335d317104ad141f
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-02-05 19:42:55 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[10]="btabb";
+    int j;
+    for(j=0;a[j]!=0;j++)
+    {
+    if(a[j]=='a'||a[j]=='e'||a[j]=='i'||a[j]=='o'||a[j]=='u')
+    
+     printf("YES");
+    
+    
+    
+    
+}
+}
