2e393b0b90c0555f75213d23e1a735bb08fce266
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-02-07 16:39:38 +0530

    Update 78.c

--- 78.c
+++ 78.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-    int a;
+    int b;
     printf("enter the number");
-    scanf("%d",&a);
-    if((a%13)==0)
+    scanf("%d",&b);
+    if((b%13)==0)
     {
         printf("yes");
     }
