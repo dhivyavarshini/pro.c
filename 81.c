1110aef5cbcda8a87542ad3be37b0ffd8cd6f7c2
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-02-09 15:45:22 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+ int kabalininja,opppnentninja,i,t;
+ for(i=0;i<3;i++)
+ {
+ scanf("%d %d",&kabalininja,&opppnentninja);
+  printf("%d ",opppnentninja-kabalininja);
+}
+  return 0;
+}
