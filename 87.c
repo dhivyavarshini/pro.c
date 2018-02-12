723e929e00403f430644351cc47a146d64febd06
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-02-12 16:15:50 +0530

    Create 87.c

---
+++ 87.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n1,n2,g,i;
+	scanf("%d %d",&n1,&n2);
+	for(i=1;i<=n1&&i<=n2;i++)
+	{
+		if(n1%i==0 && n2%i==0)
+		{
+			g=i;
+		}
+	}
+	printf("%d",g);
+	return 0;
+}
