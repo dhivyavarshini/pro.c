8d90004342933b976d2f719b4bd528149de45902
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-02-19 16:49:05 +0530

    Update 70.c

--- 70.c
+++ 70.c
@@ -1,10 +1,19 @@
 
 #include<stdio.h>
-int main()
+int main(void)
 {
-	int a,b;
-	
-	scanf("%d",&a);
-	b=a*2;
-	printf("%d",b);
+int n,c=0,g=1,p=2,i;
+printf("Enter the number:");
+scanf("%d",&n);
+while(n!=1)
+{
+n=n/2;
+c++;
+}
+for(i=0;i<c+1;i++)
+{
+g=g*p;
+}
+printf("The nearest greater power of 2 is:%d",g);
+return 0;
 }
