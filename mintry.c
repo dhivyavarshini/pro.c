fba75e0b366d5ca0ecf46bc3ee2044bd8094e1aa
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2018-02-28 17:00:38 +0530

    Create mintry.c

---
+++ mintry.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int a[40],t,min,;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d ",&a[i]);
+ 
+	}
+ 
+	min=a[0];
+	for(i=1;i<n;i++)
+	{
+	if(min<a[i])
+	{
+		min=a[i];
+	}
+	}
+	for(i=0;i<n;i++)
+	{
+	printf("%d",min);
+	return 0;
+}
